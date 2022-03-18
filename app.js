66e4e0b7b4232ab0ff1464da0eb6a3089f46c3a2
Beth <bethmelmtv@gmail.com>
2022-03-17 23:08:24 -0700

    added comment

--- app.js
+++ app.js
@@ -6,3 +6,19 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+
+
+
+//   //displayCurrentPoll()
+// //mutates DOM to display current state of current poll
+
+//   //displayAllPolls() 
+
+//   //clears out DOM and appends to poll div using current state of past polls
+
+
+//   renderPoll(poll)
+
+//   // returns DOM node
+//
