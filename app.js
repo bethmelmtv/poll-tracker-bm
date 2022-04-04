0085756686449ccbc9b0ac4be7233d07b371737b
Beth <bethmelmtv@gmail.com>
2022-04-04 01:26:38 -0700

    grabbed dom elements

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
+import { renderPoll } from '/.render-utils.js';
+
 // import functions and grab DOM elements
 
+const pollQuestionInputEl = document.querySelector('poll-question-input');
+const optionAEl = document.querySelector('option-a');
+const optionBEl = document.querySelector('option-b');
+const startThePollButton = document.querySelector('start-poll-button');
+const optionOneAdd = document.querySelector();
+const optionOneSubtract = document.querySelector();
+const optionTwoAdd = document.querySelector();
+const optionTwoSubtract = document.querySelector();
+const finishGameButton = document.querySelector('finish-game-button');
+
+const pastPollContainer = document.querySelector('past-polls-container');
+
 // let state
+
+optionOneAdd=0;
+optionOneSubtract=0;
+
+optionTwoAdd= 0;
+optionTwoSubtract= 0;
+
+
 
 // set event listeners 
   // get user input
@@ -21,4 +43,10 @@
 //   renderPoll(poll)
 
 //   // returns DOM node
-//
+
+
+
+
+// displayCurrentPoll()
+// displayAllPolls()
+// //
