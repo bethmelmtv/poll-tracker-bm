b8406bd1bdf4bb633d42dbfd3e102acd5c2f5917
Beth <bethmelmtv@gmail.com>
2022-04-06 09:41:48 -0700

    fixed elint errors

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ import { renderPoll } from './render-utils.js';
 
 // import functions and grab DOM elements
 
-const startButtonEl = document.querySelector('.start-button');
+// const startButtonEl = document.querySelector('.start-button');
 const pastpollEl = document.querySelector('.past-polls-container');
-const currentpollEl = document.querySelector('.current-poll');
+// const currentpollEl = document.querySelector('.current-poll');
 
 const form = document.querySelector('form');
 
