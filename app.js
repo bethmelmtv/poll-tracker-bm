405d4bc8c331b9c6c45bf3dc1a6d521370c0bc0a
Beth <bethmelmtv@gmail.com>
2022-04-05 23:02:09 -0700

    added html

--- app.js
+++ app.js
@@ -2,51 +2,10 @@ import { renderPoll } from '/.render-utils.js';
 
 // import functions and grab DOM elements
 
-const pollQuestionInputEl = document.querySelector('poll-question-input');
-const optionAEl = document.querySelector('option-a');
-const optionBEl = document.querySelector('option-b');
-const startThePollButton = document.querySelector('start-poll-button');
-const optionOneAdd = document.querySelector();
-const optionOneSubtract = document.querySelector();
-const optionTwoAdd = document.querySelector();
-const optionTwoSubtract = document.querySelector();
-const finishGameButton = document.querySelector('finish-game-button');
+const startButtonEl = document.querySelector('.start-button');
+const pastpollEl = document.querySelector('.past-polls-container');
+const currentpollEl = document.querySelector('current-poll');
 
-const pastPollContainer = document.querySelector('past-polls-container');
+const form = document.querySelector('form');
 
-// let state
-
-optionOneAdd=0;
-optionOneSubtract=0;
-
-optionTwoAdd= 0;
-optionTwoSubtract= 0;
-
-
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
-
-
-
-
-//   //displayCurrentPoll()
-// //mutates DOM to display current state of current poll
-
-//   //displayAllPolls() 
-
-//   //clears out DOM and appends to poll div using current state of past polls
-
-
-//   renderPoll(poll)
-
-//   // returns DOM node
-
-
-
-
-// displayCurrentPoll()
-// displayAllPolls()
-// //
+const closePollButton = document.querySelector('.close-poll');
